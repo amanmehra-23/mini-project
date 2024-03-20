4da128e588ff11a4a28b73a5ed3cea5c7448b7a8
amanmehra-23 <amanmehra6b@gmail.com>
2024-03-21 02:12:23 +0530

    flask app

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ def get_requirements(file_path:str)->List[str]:
         
 setup(
     name="mlproject",
-    version="0.0.1",
+    version="0.0.1", 
     author='Aman',
-    author_email = 'amanmehra6b@gmail.com',
+    author_email = 'amanmehra6b@gmail.com',   
     packages = find_packages(),
     install_requires= get_requirements('requirements.txt')
 )
